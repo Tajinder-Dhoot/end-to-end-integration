a2853620a41a96ddf81cc0c26adba5318f0cbd59
Tajinder Singh Dhoot <singhtazinder047@gmail.com>
2021-04-25 23:05:05 -0400

    Created automation project

---
+++ 9Jetkins/src/testcases/TC_001.java
@@ -0,0 +1,20 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+
+	@Test
+	public void tc_001() {
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://thetestingworld.com/index.php?option=com_users&view=login&Itemid=587");
+		
+		driver.findElement(By.id("username")).sendKeys("tajinder@gmail.com");
+		driver.findElement(By.id("password")).sendKeys("tajinder@gmail.com");
+		driver.close();
+	}
+}
